bf0ace49c7957c30fc3ef0b6ec4c81dac364b41f
anthamlogistics <anthamlogistics@gmail.com>
2025-03-08 18:06:12 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsyhtEFLkb7NDfo1dpbkOfx7cQyKZqlwU",
+  authDomain: "antham-logistics-admin-panel.firebaseapp.com",
+  projectId: "antham-logistics-admin-panel",
+  storageBucket: "antham-logistics-admin-panel.firebasestorage.app",
+  messagingSenderId: "329074259358",
+  appId: "1:329074259358:web:70ef86897ba7426476928f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
